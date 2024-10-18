439b30f6c4897e0322be4c65c674f75ef5c6255d
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-10-18 15:11:43 +0800

    add log viewer

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('active_applicant_at');
