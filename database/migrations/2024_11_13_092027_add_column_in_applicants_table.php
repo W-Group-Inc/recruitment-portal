e2050db738438a39b109016d16814e5cdbb23aea
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-11-14 08:55:33 +0800

    rename

--- database/migrations/2024_11_13_092027_add_column_in_applicants_table.php
+++ database/migrations/2024_11_13_092027_add_column_in_applicants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnInApplicantsTable extends Migration
+class AddColumnSourceEmployeeApplicatioInApplicantsTable extends Migration
 {
     /**
      * Run the migrations.
